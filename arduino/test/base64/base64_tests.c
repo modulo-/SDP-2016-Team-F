1a33b8fc38c19618657d34b25c2d534897338412
Thomas Kerber <thomas@tkerber.org>
2016-01-19 23:23:27 +0000

    Add a few more tests. It looks like it works.

--- arduino/test/base64/base64_tests.c
+++ arduino/test/base64/base64_tests.c
@@ -3,17 +3,35 @@
 #include <string.h>
 #include <stdio.h>
 
+struct test {
+    char *enc;
+    char *dec;
+};
+
 int main() {
-    char *ret = NULL;
-    size_t len;
-    base64Encode("test", 4, &ret, &len);
-    assert(len == 6);
-    assert(strncmp(ret, "dGVzdA", 6) == 0);
-    char *ret2 = NULL;
-    size_t len2;
-    base64Decode(ret, len, &ret2, &len2);
-    assert(len2 == 4);
-    assert(strncmp(ret2, "test", 4) == 0);
+    char *buf = NULL;
+    size_t buflen;
+    struct test tests[] = {
+        {"dGVzdA", "test"},
+        {"JCQlKkBhc2RmWU9JS0xKOg", "$$%*@asdfYOIKLJ:"},
+        {}
+    };
+    struct test *at = tests;
+    while(at->enc) {
+        base64Encode(at->dec, strlen(at->dec), &buf, &buflen);
+        assert(base64Valid(at->enc, strlen(at->enc)));
+        assert(buflen == strlen(at->enc));
+        assert(strncmp(buf, at->enc, buflen) == 0);
+        base64Decode(at->enc, strlen(at->enc), &buf, &buflen);
+        assert(buflen == strlen(at->dec));
+        assert(strncmp(buf, at->dec, buflen) == 0);
+        at++;
+    }
+    assert(!base64Valid("$", 1));
+    assert(!base64Valid("^", 1));
+    assert(!base64Valid("%", 1));
+    assert(!base64Valid("*", 1));
+    assert(!base64Valid("\n", 1));
     printf("base64 OK\n");
     return 0;
 }
