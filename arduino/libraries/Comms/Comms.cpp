a39c046115a06e2ae93106690b4633c56868606b
Margus Lind <margus.lind@gmail.com>
2016-01-20 09:39:58 +0000

    Create template for comms library

---
+++ arduino/libraries/Comms/Comms.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+
+#define MotorBoardI2CAddress 4
+
+void commsSetup() {
+  Serial.write("Comms Lib Hello");
+}
+
---
+++ arduino/libraries/Comms/Comms.h
@@ -0,0 +1,7 @@
+//SDPArduino.h
+ 
+#ifndef HEADER_COMMS
+  #define HEADER_COMMS
+	//Power is a value between 0 and 100. 
+  void commsSetup(void);	//Sets up the arduino, run inside of void setup().
+#endif
