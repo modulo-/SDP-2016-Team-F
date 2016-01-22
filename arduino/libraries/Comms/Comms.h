1f4a4931362e713f9ae04424c0f5cddc77f66d72
Margus Lind <margus.lind@gmail.com>
2016-01-22 16:59:01 +0000

    Use thomas' script to overwrite old comms

--- arduino/libraries/Comms/Comms.h
+++ arduino/libraries/Comms/Comms.h
@@ -1,7 +1,45 @@
-//SDPArduino.h
- 
-#ifndef HEADER_COMMS
-  #define HEADER_COMMS
-	//Power is a value between 0 and 100. 
-  void commsSetup(void);	//Sets up the arduino, run inside of void setup().
-#endif
+#pragma once
+#include <stddef.h>
+
+namespace comms {
+
+// The device "id" ('1' or '2').
+//
+// This is NOT defined by the comms library, but must be defined in the core
+// arduino code.
+extern const char DEVICEID;
+
+// Initializes the comms system.
+//
+// Takes strings for the channel, and the control character sequence.
+//
+// Also sets the panid and encryption keys.
+//
+// Returns true if the initialization is successful.
+//
+// Example usage:
+//
+// if(!comms::init("60", "~~~")) {
+//     // DO PANIC
+// }
+bool init(const char *chan, const char *control);
+
+// Checks for any recieved packets. If any are recieved, process will be
+// called.
+//
+// Should run with little else to delay it in the main loop.
+void poll();
+
+// Sends a packet of data to a target.
+void send(void *data, char target, size_t len);
+
+// Processes recieved data.
+//
+// This is NOT defined by the comms library, but must be defined in the core
+// arduino code.
+//
+// This recieves ownership of `data`, and *must* free it when it is no longer
+// used.
+void process(void *data, size_t len);
+
+}
