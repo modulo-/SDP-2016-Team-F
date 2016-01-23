d1930c0be680c85bbb4b52a5f9ac0161b9914d4a
Thomas Kerber <thomas@tkerber.org>
2016-01-23 23:03:41 +0000

    Fix command tester thingy, add command chaining functionality, which is not tested but *should* work.

--- src/group11cmd.py
+++ src/group11cmd.py
@@ -1,6 +1,19 @@
-#/usr/bin/python2
+#!/usr/bin/python2
 import comms
 import sys
+
+def guardedint(arg):
+    ret = int(arg)
+    if ret > 10000:
+        print "time given greater than 10 secs. ignoring."
+        return -1
+    elif ret < 0:
+        print "stop trying to break my code you asshole."
+        return -1
+    else:
+        return ret
+
+
 
 if len(sys.argv) < 2:
     print "Please provide the RF device as an argument (e.g. '/dev/ttyACM0')"
@@ -14,24 +27,33 @@ else:
     print "exit"
     print ""
     print "time is given in milliseconds. Please wait for a command to complete before sending a new one."
+    print "commands aside from 'exit' may be chained. E.g.:"
+    print ""
+    print "kicker_fwd 500 led_on 1000 kicker_bwd 500"
+    print ""
+    print "Please use this responsibly, packets are limited in effective size!
     while True:
         line = raw_input('% ')
-        if line.startswith("led_on "):
-            time = int(line[7:])
-            if time > 10000:
-                print "time given greater than 10 secs. ignoring."
-            comms.send(''.join(chr(x) for x in [0, time & 0xff, (time >> 8) & 0xff]), '1')
-
-        elif line.startswith("kicker_fwd "):
-            time = int(line[11:])
-            if time > 10000:
-                print "time given greater than 10 secs. ignoring."
-            comms.send(''.join(chr(x) for x in [1, time & 0xff, (time >> 8) & 0xff]), '1')
-        elif line.startswith("kicker_bwd "):
-            time = int(line[11:])
-            if time > 10000:
-                print "time given greater than 10 secs. ignoring."
-            comms.send(''.join(chr(x) for x in [1, time & 0xff, (time >> 8) & 0xff]), '1')
-        elif line == 'exit':
+        if line == 'exit':
             exit(0)
+        args = line.split()
+        i = 0
+        cmdbytes = []
+        while i < len(args):
+            if args[i] == 'led_on':
+                time = guardedint(args[i + 1])
+                if time != -1:
+                    cmdbytes.extend([0, time & 0xff, (time >> 8) & 0xff])
+                i += 2
+            elif args[i] == 'kicker_fwd':
+                time = guardedint(args[i + 1])
+                if time != -1:
+                    cmdbytes.extend([1, time & 0xff, (time >> 8) & 0xff])
+                i += 2
+            elif args[i] == 'kicker_bwd':
+                time = guardedint(args[i + 1])
+                if time != -1:
+                    cmdbytes.extend([2, time & 0xff, (time >> 8) & 0xff])
+                i += 2
+        comms.send(''.join(chr(x) for x in cmdbytes), '1')
 
