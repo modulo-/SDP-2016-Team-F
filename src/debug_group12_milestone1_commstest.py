2654741717fc218d70c8e61ff77ad0f1816e0321
Margus Lind <margus.lind@gmail.com>
2016-01-23 01:00:03 +0000

    milestone 1 part 3 made prettier

--- src/debug_group12_milestone1_commstest.py
+++ src/debug_group12_milestone1_commstest.py
@@ -1,16 +1,34 @@
+import math
 import comms
 
-comms.init("COM18", "67", "~~~", listen=True)
+chunk_size = 30
+serial_device = "COM18"  # or on linux /dev/ttyACM0 etc
+robot_id = 2 # group 11 - 1, group 12 - 2
+rf_channel = "67"  # hex
+guard_chars = "~~~"
+i2c_freq = 20  # MHz
+filename = "b64.py"  # file to transmit
 
 
-data = "1234567890"
+def main():
+    # comms.init(serial_device, rf_channel, guard_chars, listen=True)
+    with open(filename, mode='rb') as infile:
+        data = infile.read(250)
 
-# beware: octal
-comms.send(b"d\0\2\12", 2)
-comms.send(b"d\1\0\3abc", 2)
-comms.send(b"d\2\3\3def", 2)
-comms.send(b"d\3\6\3ghi", 2)
-comms.send(b"d\4\11\1j", 2)
+    chunk_count = int( math.ceil(len(data)*1.0/chunk_size))
 
-for i in range(5):
-    print i
+    # d[1B packet num][1B i2c freq][1B data len]
+    cmd = b"d\x00" + chr(i2c_freq) + chr(len(data))
+    # comms.send(cmd, 2)
+    print ":".join("{:02x}".format(ord(c)) for c in cmd)
+
+    for i in range(chunk_count):
+        # d[1B packet num][1B index of first byte in packet][1B packet len][data]
+        data_size = chunk_size if i != chunk_count-1 else len(data)%chunk_size
+        first_byte = i*chunk_size
+        data_str = b"d" + chr(i+1) + chr(first_byte) + chr(data_size) + "".join(data[first_byte:first_byte+data_size])
+        print ":".join("{:02x}".format(ord(c)) for c in data_str)
+        # comms.send(data_str, robot_id)
+
+if __name__ == '__main__':
+    main()
