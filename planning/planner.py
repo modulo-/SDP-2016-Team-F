4d47b95d985da10fdf97f119ab50830ae3365d3c
Hugh McGrade <hugh_mcgrade@hotmail.co.uk>
2016-02-06 17:33:20 +0000

    Add main planner code

---
+++ planning/planner.py
@@ -0,0 +1,24 @@
+from comms import CommsManager
+from models import *
+
+class Planner:
+
+    def __init__(self):
+        self.comms = [CommsManager(0)]
+
+    # Get goal for robot in world
+    def get_goal(self, world, robot):
+        raise NotImplemented
+
+    # Perform actions
+    def actuate(self, *actions):
+        for a in actions:
+            a.perform(self.comms)
+
+    # Make plans for each robot and perform them
+    def plan_and_act(self, world):
+        # For now plan for just one robot
+        robot = world.our_attacker()
+        goal = get_goal(world, robot)
+        action = goal.generate_action()
+        actuate(action)
