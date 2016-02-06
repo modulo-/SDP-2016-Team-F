ec328af79f4d685721163a107425375209293f1d
Hugh McGrade <hugh_mcgrade@hotmail.co.uk>
2016-02-06 19:30:25 +0000

    Add same action test to plan and act

--- planning/planner.py
+++ planning/planner.py
@@ -5,15 +5,16 @@ class Planner:
 
     def __init__(self):
         self.comms = [CommsManager(0)]
+        self.robot_actions = [None]
 
     # Get goal for robot in world
     def get_goal(self, world, robot):
         raise NotImplemented
 
     # Perform actions
-    def actuate(self, *actions):
-        for a in actions:
-            a.perform(self.comms)
+    def actuate(self, actions):
+        for i in range(len(actions)):
+            actions[i].perform(self.comms[i])
 
     # Make plans for each robot and perform them
     def plan_and_act(self, world):
@@ -22,4 +23,8 @@ class Planner:
         goal = get_goal(world, robot)
         action = goal.generate_action()
         messages = action.get_messages()
-        actuate(action)
+        actions = [None]
+        if action != self.robot_actions[0]:
+            actions[0] = action
+        self.robot_actions = actions
+        actuate(actions)
