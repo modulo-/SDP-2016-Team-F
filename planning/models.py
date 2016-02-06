bbad9182dcb3ac96e4fc94e4894694a1222b98ba
Hugh McGrade <hugh_mcgrade@hotmail.co.uk>
2016-02-06 17:25:15 +0000

    Comment and refactor models

--- planning/models.py
+++ planning/models.py
@@ -1,22 +1,26 @@
 import utils
 
+# Base class for goals
 class Goal (object):
     def __init__(self, world, robot):
         self.world = world
         self.robot = robot
 
+    # Return the next action necesary to achieve the goal
     def generate_action(self):
         raise NotImplemented
 
+# Go to and grab ball
 class GetBall (Goal):
     def generate_action(self):
-        actions = [CatchBall(self, self.world, self.robot),
+        actions = [GrabBall(self, self.world, self.robot),
                    GoToStaticBall(world, self.robot)]
         for a in actions:
             if a.is_possible():
                 return a
         return None
 
+# Base class for actions
 class Action (object):
     preconditions = []
 
@@ -24,12 +28,14 @@ class Action (object):
         self.world = world
         self.robot = robot
 
+    # Test the action's preconditions
     def is_possible(self):
         for condition in self.preconditions:
             if not condition(world):
                 return False
         return True
 
+    # Do comms to perform action
     def perform(self, comms):
         raise NotImplemented
 
@@ -39,7 +45,7 @@ class GoToStaticBall (Action):
     def perform(self, comms):
         comms.move_to(world.ball.x, world.ball.y)
 
-class CatchBall (Action):
+class GrabBall (Action):
     preconditions = [lambda w, r: r.can_catch_ball(w.ball)]
 
     def perform(self, comms):
