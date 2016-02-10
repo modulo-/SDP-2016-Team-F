c63e3f0ea59e0e070150c672a130c2579cd5090e
siivanov <simeon.iv.ivanov@gmail.com>
2016-02-10 01:20:47 +0000

    Add vision thread and correct timer to main script

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from Vision.vision import Vision
 from planning.planner import Planner
 from planning.comms import CommsManager
 from planning.world import World
-from threading import Timer
+from threading import Timer, Thread
 
 PITCH_NO = 0
 
 latest_world = World('left', PITCH_NO)
+latest_world.our_robot._receiving_area = {'width': 30, 'height': 30, 'front_offset': 12}
+
 
 def new_vision(world):
     latest_world.update_positions(
@@ -15,13 +17,18 @@ def new_vision(world):
             "ball": world.ball
         }
     )
-
-vision = Vision(video_port=0, pitch=PITCH_NO, planner_callback=new_vision)
-comms = RFCommsManager(0, "/dev/ttyACM[NUMBER]")
+    
+def start_vision():
+	vision = Vision(video_port=0, pitch=PITCH_NO, planner_callback=new_vision)
+	
+thread = Thread(target=start_vision)
+thread.start()
+comms = CommsManager(0)
 planner = Planner(comms=comms)
 
 def run_planner():
     planner.plan_and_act(latest_world)
+    timer = Timer(1, run_planner)
     timer.start()
     
 timer = Timer(1, run_planner)
