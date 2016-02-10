db1ac22b0baea9c00b4250ae9e3efb56d9ca1834
siivanov <simeon.iv.ivanov@gmail.com>
2016-02-10 01:25:40 +0000

    Planning code I didn't do

--- Vision/calibrations.py
+++ Vision/calibrations.py
@@ -4,24 +4,25 @@ colour_profiles = {
     "breadnel":
     {
         "red": {
-            "min": np.array([106, 150, 120]),
+            "min": np.array([138, 229, 0]),
             "max": np.array([255, 255, 255])
         },
         "yellow": {
-            "min": np.array([15, 54, 225]),
-            "max": np.array([42, 255, 255])
+            "min": np.array([20, 192, 192]),
+            "max": np.array([40, 255, 255])
         },
         "blue": {
-            'max': np.array([91, 255, 238]),
-            'min': np.array([76, 86, 158])
+            'min': np.array([81, 110, 0]),
+            'max': np.array([130, 255, 255])
+            
         },
         "green": {
-            'min': np.array([60, 66, 200]),
-            'max': np.array([76, 130, 255])
+            'min': np.array([35, 159, 223]),
+            'max': np.array([81, 255, 255])
         },
         "pink": {
-            'min': np.array([121, 59, 219]),
-            'max': np.array([170, 255, 255])
+            'min': np.array([121, 130, 220]),
+            'max': np.array([194, 255, 255])
         }
     },
     "3d04_pc1":
@@ -35,7 +36,7 @@ colour_profiles = {
             "max": np.array([42, 255, 255])
         },
         "blue": {
-        	'min': np.array([70, 76, 101]),
+        	'min': np.array([70, 96, 101]),
             'max': np.array([141, 255, 255])
             
         },
@@ -51,24 +52,25 @@ colour_profiles = {
     "3d04_pc4":
     {
         "red": {
-            "min": np.array([106, 150, 120]),
-            "max": np.array([255, 255, 255])
+            "min": np.array([0, 178, 148]),
+            "max": np.array([5, 255, 255])
         },
         "yellow": {
-            "min": np.array([15, 54, 225]),
+            "min": np.array([21, 172, 181]),
             "max": np.array([42, 255, 255])
         },
         "blue": {
-            'max': np.array([91, 255, 238]),
-            'min': np.array([76, 86, 158])
+        	'min': np.array([85, 79, 116]),
+            'max': np.array([128, 255, 255])
+            
         },
         "green": {
-            'min': np.array([60, 66, 200]),
-            'max': np.array([76, 130, 255])
+            'min': np.array([50, 180, 151]),
+            'max': np.array([65, 255, 255])
         },
         "pink": {
-            'min': np.array([121, 59, 219]),
-            'max': np.array([170, 255, 255])
+            'min': np.array([115, 124, 76]),
+            'max': np.array([190, 255, 255])
         }
     }
 }
