a5d69d54fda81e8cc80f993ce7f27b369c41c319
Hugh McGrade <hugh_mcgrade@hotmail.co.uk>
2016-02-19 16:01:13 +0000

    Fix main.py, replacing our_robot with our_defender

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ from sys import argv
 PITCH_NO = 0
 
 latest_world = World('left', PITCH_NO)
-latest_world.our_robot._receiving_area = {'width': 40, 'height': 50, 'front_offset': 20}
+latest_world.our_defender._receiving_area = {'width': 40, 'height': 50, 'front_offset': 20}
 
 
 def new_vision(world):
     latest_world.update_positions(
         {
-            "our_robot": world.robot_blue_green,
+            "our_defender": world.robot_blue_green,
             "ball": world.ball,
         }
     )
-    if latest_world.our_robot.is_missing():
+    if latest_world.our_defender.is_missing():
         print ("Robot is missing!")
     
 def start_vision():
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     planner.set_task(task.strip())
 
     def run_planner():
-        print (latest_world.our_robot.angle)
+        print (latest_world.our_defender.angle)
         planner.plan_and_act(latest_world)
         timer = Timer(1, run_planner)
         timer.start()
