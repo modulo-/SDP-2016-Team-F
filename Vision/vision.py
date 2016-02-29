9e1cd299a218ff2647b6ab7fc208d345e4c8205a
Hugh McGrade <s1305395@sms.ed.ac.uk>
2016-02-29 14:27:40 +0000

    Fix pitch number use in vision.py

--- Vision/vision.py
+++ Vision/vision.py
@@ -24,19 +24,18 @@ from scipy.spatial import distance as sp_dist
 class Vision:
     def __init__(self, video_port=0, pitch=None, planner_callback=None):
         self.config = Config(self)
-
         while pitch is None:
             req_room = raw_input("Enter Pitch Room [{opts}]: ".format(opts="/".join(self.config.pitch_room.options)))
             pitch_no = self.config.pitch_room.getCode(req_room, unifier=lambda str: re.sub(r'\W+', '', str.upper()))
             if pitch_no is not None:
                 pitch = pitch_no
-                self.config.pitch_room.selected = pitch
-                if pitch == 0:
-                    self.config.colours = colour_profiles['pitch_3d03']
-                elif pitch == 1:
-                    self.config.colours = colour_profiles['pitch_3d04']
-        #    else:
-                #print("Try again")
+
+        
+        self.config.pitch_room.selected = pitch
+        if pitch == 0:
+            self.config.colours = colour_profiles['pitch_3d03']
+        elif pitch == 1:
+            self.config.colours = colour_profiles['pitch_3d04']
 
         self.cam = Camera(port=video_port, pitch=pitch, config=self.config)
         #print("Camera initialised")
